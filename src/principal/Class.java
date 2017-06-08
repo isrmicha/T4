c3a2c17129c77a27b3f59707c3426b128f70e44f
Israel <isrmicha@gmail.com>
2017-06-08 14:41:05 -0300

    e

--- src/principal/Class.java
+++
@@ -1,9 +0,0 @@
-package principal;
-
-public class Class {
-	public static void main(String[] args){
-		Funcionario f = new Funcionario("Davi", 2);	
-		System.out.print(f.getNome());
-		System.out.print(f.getSalario());		
-	}
-}
---
+++ src/teste/teste.java
@@ -0,0 +1,18 @@
+package teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import principal.Funcionario;
+
+public class teste {
+	Funcionario f;
+	
+	@Test
+	public void test() {
+		
+		assertEquals(f = new Funcionario("Davi",2),false);
+	}
+
+}
