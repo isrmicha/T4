884787bdf2c951804f3c2c7c2f739aa136ba8e68
Israel <isrmicha@gmail.com>
2017-06-08 14:09:09 -0300

    e

---
+++ src/principal/AumentoAbsurdoException.java
@@ -0,0 +1,7 @@
+package principal;
+
+public class AumentoAbsurdoException extends RuntimeException {
+	public AumentoAbsurdoException (String msg){
+		super(msg);
+	}
+}
--- src/principal/Funcionario.java
+++ src/principal/Funcionario.java
@@ -1,48 +1,65 @@
 package principal;
 
 public class Funcionario {
-	
+
 	private int matricula;
 	private String nome;
 	private double salario;
-	
-	public Funcionario(String nome, double salario){
+	static int matriculas = 0;
+
+	public Funcionario(String nome, double salario) {
 		try {
-			if(nome.length() > 40 || salario < 350){
-				if(nome.length() > 40){
+			if (nome.length() > 40 || salario < 350) {
+				if (nome.length() > 40) {
 					throw new IllegalArgumentException("Nome muito grande!");
 				}
-				if(salario < 350){
+				if (salario < 350) {
 					throw new ViolacaoCltException("Salário baixo");
 				}
-			}else{
+			} else {
 				this.nome = nome;
 				this.salario = salario;
+				this.matricula = ++Funcionario.matriculas;
 			}
-		}catch(ViolacaoCltException e){
+		} catch (ViolacaoCltException e) {
 			e.getMessage();
 			e.printStackTrace();
 		} catch (IllegalArgumentException e) {
 			e.getMessage();
 			e.printStackTrace();
-		}		
+		}
 	}
-	
-	//Getter e Setters
+
 	public int getMatricula() {
 		return matricula;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public double getSalario() {
 		return salario;
 	}
+
 	public void setSalario(double salario) {
-		this.salario = salario;
+		if(salario>0){
+			try{
+				if(salario>1.1*(this.salario)){
+					throw new AumentoAbsurdoException("Aumento acima de 10% !");
+				}
+			} 
+			catch (AumentoAbsurdoException e) {
+			e.getMessage();
+			e.printStackTrace();
+		}	
+		
+		
 	}
 	
+	}
 }
